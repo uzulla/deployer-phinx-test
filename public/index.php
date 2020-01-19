cfeccd56e06ddfb52f56b6eb9f51736e0ace1ba9
uzulla <uzulla@himitsukichi.com>
2020-01-19 14:03:11 +0900

    add sample

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+const __BASE_DIR__ = __DIR__ . "/../";
+
+require_once __BASE_DIR__ . "/vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__BASE_DIR__);
+$dotenv->load();
+
+$sqlite_database_path = __BASE_DIR__ . ".". getenv("PHINX_ENV_PRODUCTION_NAME").".sqlite3";
+$pdo = new \PDO("sqlite:{$sqlite_database_path}");
+
+$stmt = $pdo->prepare("select * from user_logins");
+$stmt->execute();
+var_dump($stmt->fetchAll());
